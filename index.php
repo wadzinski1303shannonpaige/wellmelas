3738e4adbfd48208bce12fbb2348ddbe0f2d5870
wadzinski1303shannonpaige <wadzinski1303shannonpaige@gmail.com>
2025-04-08 19:46:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<?php
-require_once('geoplugin.class.php');
-$geoplugin = new geoPlugin();
-$geoplugin->locate();
-// create a variable for the country code
-$var_country_code = $geoplugin->countryCode;
-// redirect based on country code:
-if ($var_country_code == "US") {
-   header('Location: https://facebook.com/');
-}
-else if ($var_country_code == "NL") {
-   header('Location: https://wikipedia.org/');
-} else {
-   header('Location: https://google.com/');
-}
+<?php 
+     header('Location: https://www.facebook.com');
+     exit();
 ?>
